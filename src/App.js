84620fe8b6819281b2d06950778b112e51e1aec2
mlrivera123 <mlrivera@bu.edu>
2023-01-06 15:09:52 -0800

    Basic First Project using React

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
 import logo from './logo.svg';
 import './App.css';
 
+
+function Nav(){
+  return(
+  <nav className="nav">
+  <img src={logo} className="App-logo" alt="logo" />
+  <ul className="nav-item">
+    <li>Pricing</li>
+    <li>About</li>
+    <li>Contact</li>
+  </ul>
+</nav>
+  )
+}
+
+function Header(){
+  return(
+    <header className="head">
+    <h1>Reasons I'm Excited to Learn React</h1>
+    </header>
+  )
+}
+
+
+function Body(){
+  return(
+    <ol className="list">
+      <li>It's a popular library so i'll fit in with the cool kids!</li>
+      <li>I'm more likely to get a job as a developer if I know react.</li>
+      <li>It is actually pretty fun to learn!</li>
+    </ol>
+  )
+}
+
+function Footer(){
+  return(
+    <footer className="foot">
+      <small>© 2023 Max Rivera development. All rights reserved.</small>
+    </footer>
+  )
+}
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    <Nav/>
+    <Header/>
+    <Body/>
+    <Footer/>
     </div>
   );
 }
